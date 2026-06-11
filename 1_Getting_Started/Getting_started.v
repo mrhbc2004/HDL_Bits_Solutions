bfc519a468c305c3c33a638da46808c58c95f2e8
mrhbc2004 <mrhbc2004@gmail.com>
2026-06-11 22:09:59 +0530

    Added solutions for first section

---
+++ 1_Getting_Started/Getting_started.v
@@ -0,0 +1,6 @@
+module top_module(output one);
+    
+    // The output signal is always latched to 1'b1
+    assign one = 1'b1;
+
+endmodule
---
+++ 1_Getting_Started/output_zero.v
@@ -0,0 +1,7 @@
+module top_module(
+    output zero
+);
+    // The output signal is always latched to 1'b0
+    assign zero = 1'b0;
+
+endmodule
