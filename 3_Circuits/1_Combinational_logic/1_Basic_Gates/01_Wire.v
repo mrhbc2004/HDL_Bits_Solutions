fd7df2319b27c0e872fcaef2fd102f73463cab0d
mrhbc2004 <mrhbc2004@gmail.com>
2026-06-14 10:14:45 +0530

    Updated the names from 1_ to 01_

---
+++ 3_Circuits/1_Combinational_logic/1_Basic_Gates/01_Wire.v
@@ -0,0 +1,8 @@
+module top_module (
+    input in,
+    output out);
+    
+    // just continuous assignment of in to out
+	assign out = in;
+    
+endmodule
---
+++ 3_Circuits/1_Combinational_logic/1_Basic_Gates/02_GND.v
@@ -0,0 +1,5 @@
+module top_module (
+    output out);
+    // latching the out signal to low signal (1'b0)
+	assign out = 1'b0;
+endmodule
