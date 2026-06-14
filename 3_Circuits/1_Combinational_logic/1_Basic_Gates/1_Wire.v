ff3cc903bac2b8d594781febb41b52f0988af1d3
mrhbc2004 <mrhbc2004@gmail.com>
2026-06-14 10:17:43 +0530

    Deleted the older files

--- 3_Circuits/1_Combinational_logic/1_Basic_Gates/1_Wire.v
+++
@@ -1,8 +0,0 @@
-module top_module (
-    input in,
-    output out);
-    
-    // just continuous assignment of in to out
-	assign out = in;
-    
-endmodule
--- 3_Circuits/1_Combinational_logic/1_Basic_Gates/2_GND.v
+++
@@ -1,5 +0,0 @@
-module top_module (
-    output out);
-    // latching the out signal to low signal (1'b0)
-	assign out = 1'b0;
-endmodule
