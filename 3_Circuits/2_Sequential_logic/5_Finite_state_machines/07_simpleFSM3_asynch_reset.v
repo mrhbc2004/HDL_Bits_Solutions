9d638566d09c58ac6b454a3a5a8bcf012d4b8e9f
mrhbc2004 <mrhbc2004@gmail.com>
2026-06-14 18:28:54 +0530

    Added FSM sections 9 solutions

---
+++ 3_Circuits/2_Sequential_logic/5_Finite_state_machines/07_simpleFSM3_asynch_reset.v
@@ -0,0 +1,28 @@
+module top_module(
+    input clk,
+    input in,
+    input areset,
+    output out); //
+
+    parameter A = 0,B = 1,C = 2,D = 3;
+    reg [1:0] state,next;
+    // State transition logic
+    always @* begin
+        case(state)
+            A: next = in ? B : A;
+            B: next = in ? B : C;
+            C: next = in ? D : A;
+            D: next = in ? B : C;
+        endcase
+    end
+
+    // State flip-flops with asynchronous reset
+    always @(posedge clk or posedge areset) begin
+        if(areset) state <= A;
+        else state <= next;
+    end
+    assign out = (state == D);
+
+    // Output logic
+
+endmodule
